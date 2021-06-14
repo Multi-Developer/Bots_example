6ac3a3932de41633b1768f2c10c43565cbb7e6cd
Multi-Developer <multi-developer@yandex.ru>
2021-06-14 18:24:38 +0300

    ФССП api methods created

---
+++ task_mts.py
@@ -0,0 +1,377 @@
+import os
+import tkinter as tk
+from time import sleep
+from tkinter import filedialog, ttk  # noqa
+import logging
+import pandas as pd
+import requests
+import xlsxwriter
+from PIL import ImageTk, Image
+from dotenv import load_dotenv
+from page_objects import PageObject, PageElement  # noqa
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+load_dotenv()
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+# todo создать
+class CourtPage(PageObject):
+    pass
+
+
+class Court:
+    COURT_URL = 'https://sudrf.ru/index.php?id=300#sp'
+
+    def __init__(self):
+        pass
+
+
+class FsspAPI:
+    """
+    Base class for access to API ФССП
+    docs https://api-ip.fssp.gov.ru/
+    """
+    FSSP_URL = 'https://fssp.gov.ru/'
+    URL_FSSP_API = 'https://api-ip.fssp.gov.ru/api/v1.0/'
+    REGIONS = tuple([i for i in range(1, 79)] + [86, 89, 91, 92])  # regions list
+
+    def __init__(self, strftime_mask: str = '%d.%m.%Y'):
+        self.strftime_mask = strftime_mask
+        self.fssp_tasks = []
+        self.fssp_data = []
+
+    def teimestamp_to_string(self, timestamp, mask: str = None) -> str:  # noqa
+        """
+        Timestamp pandas to string
+        :param timestamp: Timestamp typ of pandas
+        :param mask: mask to convert timestamp
+        :return str: datetime in string
+        """
+        timestamp.to_pydatetime()
+        return timestamp.strftime('%d.%m.%Y') if mask is None else timestamp.strftime(mask)
+
+    def get_full_fio(self, obj: list) -> str:  # noqa
+        """Get ful last name first name patronymic as string"""
+        return f'{obj[0]} {obj[1]} {obj[2]}'
+
+    def get_full_fio_and_birthday(self, obj: list, mask_datetime: str = None) -> str:
+        """
+        Get ful last name first name patronymic and birthday as string
+        :param obj: list with attributes
+        :return str:
+        """
+        if len(obj) > 3:
+            fio = self.get_full_fio(obj)
+            birthday = self.teimestamp_to_string(obj[3], mask_datetime)
+            return '{} {}'.format(fio, birthday)
+        return self.get_full_fio(obj)
+
+    def api_full_url(self, add_url: str) -> str:
+        """Get full url for api source
+        :param add_url: additional url value
+        :return str: full url to access url
+        """
+        return self.URL_FSSP_API + add_url
+
+    def get_status_api_fssp(self, task: str = None):  # noqa
+        """
+        Get task status from API. Call method get_result_api_fssp()
+        if code status in allowed
+        """
+        total_url = self.api_full_url('status')
+        payload = {'token': os.getenv('FSSP_TOKEN')}
+
+        for i in range(len(self.fssp_tasks)):
+
+            task = self.fssp_tasks.pop()
+            payload.update({'task': task})
+            r = requests.get(total_url, params=payload)
+
+            if r.status_code == 200:
+                # status == 0 means task is successful
+                status = r.json()['code']
+
+                if status == 0 or status == 3:
+                    self.get_result_api_fssp(task)
+
+    def get_result_api_fssp(self, task: str) -> None:
+        """
+        Get result of task from API
+        :param task: unique task value
+        :return None:
+        """
+        total_url = self.api_full_url('result')
+        payload = {'token': os.getenv('FSSP_TOKEN'),
+                   'task': task}
+        r = requests.get(total_url, payload)
+        if r.status_code == 200:
+            result = r.json()['response']['result']
+
+            for element in result:
+                element_result = element['result']
+                if element_result:
+                    for result in element_result:
+                        self.fssp_data.append({
+                            'ФИО': result['name'],
+                            'Приказ': result['exe_production'],
+                            'Детально': result['details'],
+                            'Цель': result['subject'],
+                            'Пристав': result['bailiff'],
+                            'Доп': result['ip_end'],
+
+                        })
+
+    def post_search_group(self, excel_data: list) -> None:
+        """
+        Send POST request to API
+
+        type=1 - Отправить запрос на поиск физического лица;
+        type=2 - Отправить запрос на поиск юридического лица;
+        type=3 - Отправить запрос на поиск юридического лица;
+
+        :param excel_data: list с элементами на проверку
+        :return: None
+        """
+        if excel_data is None:
+            return
+        total_url = self.api_full_url('search/group')
+
+        data_params = []
+        data = {
+            'token': os.getenv('FSSP_TOKEN'),
+        }
+
+        for person in excel_data:
+            last_name = person[0]
+            first_name = person[1]
+            second_name = person[2]
+            birthdate = self.teimestamp_to_string(person[3])
+            for region in self.REGIONS:
+                data_params.append({
+                    'type': 1,  # see docs to method
+                    'params': {
+                        'region': region,
+                        'firstname': first_name,
+                        'lastname': last_name,
+                        'secondname': second_name,
+                        'birthdate': birthdate,
+                    }
+                })
+
+                # request can't be greater than 50 in len() or if region last in list
+                if len(data_params) >= 49 or region == self.REGIONS[len(self.REGIONS) - 1]:
+                    data['request'] = data_params
+
+                    while True:
+                        r = requests.post(total_url, json=data)
+                        if r.status_code == 200:
+                            self.fssp_tasks.append(r.json()['response']['task'])
+                            data['request'].clear()
+                            logging.info('Successfully request')
+                            sleep(5)
+                            break
+                        elif (r.status_code == 429
+                              and r.json()['exception'] == 'Дождитесь результата предыдущего группового запроса'):
+                            logging.info('Need wait. r.status_code == 429')
+                            sleep(5)
+
+                        # minimal time for waiting between requests is 5 sek
+                        sleep(5)
+
+
+class Application(tk.Frame, FsspAPI, Court):
+
+    def __init__(self, root=None):
+        tk.Frame.__init__(self, root)
+        FsspAPI.__init__(self)
+        Court.__init__(self)
+
+        self.excel_data = None
+        self.court_result = True  # флаг о проверке всех запросов на court
+
+        # Tkinter GUI
+        self.root = root
+        self.canvas = tk.Canvas(self.root, width=800, height=500)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.canvas.pack()
+
+        self.label = tk.Label(root, text='Специально для МТС Банка')
+        self.label.config(font=('Arial', 20))
+        self.canvas.create_window(400, 50, window=self.label)
+
+        # Tkinter GUI
+
+        # Кнопки
+        # Загрузка excel
+        self.button_load_excel = tk.Button(text='Загрузите файл excel', command=self.load_excel,
+                                           bg='green', fg='white', font=('helvetica', 12, 'bold'))
+        self.canvas.create_window(400, 180, window=self.button_load_excel)
+
+        # Судопроизводство
+        # https://sudrf.ru/index.php?id=300#sp
+        self.button_court = tk.Button(self.root, text='Запрос в суды',
+                                      # command=self.get_court,  # todo временно удалено- метод не доработан
+                                      bg='red',
+                                      font=('helvetica', 11, 'bold'))
+        self.canvas.create_window(400, 220, window=self.button_court)
+
+        # ФССП
+        self.button_fssp = tk.Button(self.root, text='Запрос в ФССП', command=self.run_api_fssp, bg='red',
+                                     font=('helvetica', 11, 'bold'))
+        self.canvas.create_window(400, 260, window=self.button_fssp)
+
+        # exit
+        self.button_exit = tk.Button(self.root, text='Выход', command=self.root.destroy, bg='green',
+                                     font=('helvetica', 11, 'bold'))
+        self.canvas.create_window(400, 300, window=self.button_exit)
+
+    def launch_selenium(self, in_background: bool = True):
+        """
+        Запук selenium
+        :param in_background: bool возможность запуска в фоне. True- запуск в фоновом режиме, False- не в фоне
+        :return:
+        """
+        self.options = webdriver.FirefoxOptions()
+
+        # options для загрузки в фоновом режиме
+        if in_background:
+            self.options.add_argument("--headless")
+
+        self.browser = webdriver.Firefox(
+            executable_path=GeckoDriverManager().install(),
+            options=self.options
+        )
+        self.browser.implicitly_wait(5)
+        sleep(1.5)
+        return self.browser
+
+    def load_excel(self) -> None:
+        """
+        Load and parse excel. Launch by button
+        """
+        import_file_path = filedialog.askopenfilename()
+        df = pd.read_excel(import_file_path, engine="odf", usecols=['Фамилия', 'Имя', 'Отчество', 'Дата рождения'])
+        self.excel_data = df.values
+
+    def create_excel(self, data: list, file_name: str) -> None:  # noqa
+        """
+        Create excel file in the same directory, as current file
+
+        example of data to load:
+        data = [{'key1': 'value1', 'key2': 'value2'... }]
+
+        :param data: list with dict values to write into excel
+        :param file_name: str full file name with extension. Only excel format excepted
+
+        :return: None
+        """
+        directory_path = os.getcwd()
+        workbook = xlsxwriter.Workbook(directory_path + '/' + file_name)
+        worksheet = workbook.add_worksheet()
+
+        keys = list(data[0].keys())
+        keys.sort()
+
+        # create headers
+        for idx, key in enumerate(keys):
+            worksheet.write(0, idx, key)
+
+        row = 0
+        for element in data:
+            row += 1
+            for idx, key in enumerate(keys):
+                worksheet.write(row, idx, element[key])
+
+        workbook.close()
+        logging.info('Excel created!')
+
+    def run_api_fssp(self) -> None:  # noqa
+        """
+        Send request to API, get data and create excel file.
+        Working by push button
+        """
+        self.post_search_group(self.excel_data)
+        self.get_status_api_fssp()
+        self.create_excel(data=self.fssp_data, file_name='result_fssp_api.xlsx')
+
+    def run_court(self):
+        """
+        Нужно:
+        Получить на вход таблицу Excel (колонки Фамилия, Имя, Отчество) и построчно получить
+        информацию по судебным делам федеральных судов общей юрисдикции в г. Москва (https://sudrf.ru/index.php?id=300#sp).
+        """
+        browser = self.launch_selenium()
+        # TODO добавить url
+        response = browser.get(self.COURT_URL)
+        # 1. Построчечено читаем файл
+        # 2. отправляем форму
+
+        browser.close()
+        pass
+
+    # TODO неиспользуемые наработки
+
+    def get_fssp(self):
+        '''
+        https://selenium-python.readthedocs.io/
+        https://selenium-python.readthedocs.io/navigating.html#filling-in-forms
+        Нужно:
+        Получить на вход таблицу Excel (колонки Фамилия, Имя, Отчество, Дата рождения)
+        и построчно получить информацию по исполнительным производствам из сайта ФССП (http://fssprus.ru/).
+        '''
+        # 1. Построчечено читаем файл
+        # 2. отправляем форму
+        # 3. скрапим результат
+        # 4. повторяем цикл с пункта 1
+        # проверка наличия загруженной информации из ексель
+        if not self.excel_data:
+            return
+
+        # при запуске браузера сделать проверку на возможно модальное окно
+
+        # todo переместить все элементы в экземпляр класса
+        browser = self.launch_selenium()
+        sleep(1)
+        close_modal = browser.find_element_by_xpath('//button[@class="tingle-modal__close"]')
+        if close_modal:
+            close_modal.click()
+        sleep(1)
+        for value in self.excel_data:
+            # должны быть заполнены все 4 ячейки в excel
+            if len(value) < 4:
+                continue
+            browser.get(self.FSSP_URL)
+            blank = browser.find_element_by_id('debt-form01')
+            button = browser.find_element_by_xpath('//button[@class="btn btn-primary"]')
+            blank.sendKeys(value.join())
+            button.click()
+            sleep(2)
+            # TODO переход на след страничку
+            # 1. Основная таблица
+            # table[@class='list border table alt-p05']
+            if browser.find_element_by_xpath('table[@class="list border table alt-p05"]'):
+                pass
+
+                # 2. Внутри таблицы разбиты на три класса:
+                # class = " "
+                # class = "even "
+                # class = "even bottom" -  только нижний элемент
+
+            browser.delete_all_cookies()  # удаление куков
+        browser.close()
+
+    def create_image(self, img):
+        """Отображение капчи"""
+        img = ImageTk.PhotoImage(Image.open(img))
+        self.canvas.create_image(20, 20, image=img)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Application(root=root)
+    app.mainloop()
